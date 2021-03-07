09d174115cb3e48c27ffcd922a6d30280ed0c066
KHLIMNOTEBOOK\khlim\eng\sourcetree <a47165705@gmail.com>
2021-03-07 18:15:26 +0900

    update : lambda index

--- lambda/index.js
+++ lambda/index.js
@@ -1,35 +1,38 @@
 const AWS = require('aws-sdk');
 const Sharp = require('sharp');
 
-const S3 = new AWS.SecretsManager({region: 'ap-northeast-2'});
+const S3 = new AWS.S3({ region: 'ap-northeast-2' });
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3-khlim
-  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12341234_abc.png
-  console.log(Bucket, Key);
+  const Bucket = event.Records[0].s3.bucket.name;
+  const Key = event.Records[0].s3.object.key;
   const filename = Key.split('/')[Key.split('/').length - 1];
-  const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
-  console.log('filename', filename, 'ext', ext);
-
+  const ext = Key.split('.')[Key.split('.').length - 1];
+  console.log(Key, filename, ext);
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
   try {
-    const s3Object = await S3.getObject({ Bucket, Key }).promise();
+    const s3Object = await S3.getObject({ // S3에서 이미지를 받아 옵니다.
+      Bucket,
+      Key,
+    }).promise();
     console.log('original', s3Object.Body.length);
     const resizedImage = await Sharp(s3Object.Body)
-      .resize(800, 800, { fit: 'inside' })
+      .resize(800, 800, {
+        fit: 'inside',
+      })
       .toFormat(requiredFormat)
       .toBuffer();
-    await S3
-      .putObject({
-        Bucket,
-        Key: `thumb/${filename}`,
-        Body: resizedImage,
-      })
-      .promise();
-    console.log('put ', resizedImage.length);
+    console.log('resize', resizedImage.length);
+    await S3.putObject({
+      Body: resizedImage,
+      Bucket,
+      Key: `thumb/${filename}`,
+    }).promise();
+    console.log('put');
     return callback(null, `thumb/${filename}`);
   } catch (error) {
     console.error(error);
     return callback(error);
   }
 };
+
